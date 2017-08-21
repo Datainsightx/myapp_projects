8f192152b04a65c362fe33bc24bcd1e7348ed15f
Isaac-moore <alabisheyi2001@yahoo.com>
2017-08-21 22:08:39 +0100

    Update upload.php

--- Templates/upload.php
+++ Templates/upload.php
@@ -2,12 +2,6 @@
 
 $target_dir = "/Upload";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$host = "host=";
-$port = "port = 5432";
-$dbname = "dbname = ";
-$credentials = "user =  password=";
-
-$db = pg_connect( "$host $port $dbname $credentials"  );
 
 $query = "INSERT INTO school_data VALUES ('$_POST[schoolid]','$_POST[target_file]') 
 $result = pg_query($query); 
