92d5095c7eb61462479d03f6b955db9060ed61a4
Isaac-moore <alabisheyi2001@yahoo.com>
2017-08-21 22:04:54 +0100

    Update upload.php

--- Templates/upload.php
+++ Templates/upload.php
@@ -8,12 +8,6 @@ $dbname = "dbname = ";
 $credentials = "user =  password=";
 
 $db = pg_connect( "$host $port $dbname $credentials"  );
-if(!$db) {
-   echo "Error : Unable to open database\n";
-   } 
-   else {
-      echo "Opened database successfully\n";
-   }
 
 $query = "INSERT INTO school_data VALUES ('$_POST[schoolid]','$_POST[target_file]') 
 $result = pg_query($query); 
